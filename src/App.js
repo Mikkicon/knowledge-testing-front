99b903707e4d54f32c79a96e7b7114fbbcde85f4
mikkicon <mikkicon@gmail.com>
2019-07-08 19:40:02 +0300

    initial setup with routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Login from "./modules/Login";
+import Register from "./modules/Register";
+/*
+/
+/users
+/users/:id
+/login
+/resgister
+/tests
+/tests/:id
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={Login} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/modules/Login.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <ul>
+            <li>Ass</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
