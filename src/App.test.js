7f79a0964d8a37fd916fcb470979a2fd0f47b2f7
mikkicon <mikkicon@gmail.com>
2019-07-12 19:19:49 +0300

    ADDED MISSING TESTS
    ✓ renders without crashing (34ms)
    ✓ Login renders without crashing (2ms)
    ✓ Register renders without crashing (1ms)
    ✕ Landing renders without crashing (35ms)
    ✓ MagnifyBadge renders without crashing (1ms)
    ✓ SearchBar renders without crashing (1ms)
    ✕ TestOnLanding renders without crashing (6ms)
    ✓ Test renders without crashing (19ms)
    
    Test Suites: 1 failed, 1 total
    Tests:       2 failed, 6 passed, 8 total
    Snapshots:   0 total
    Time:        2.351s

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import Landing from "./modules/Landing";
-import Test from "./modules/Test";
-import Login from "./modules/Login";
-import Navbar from "./modules/Navbar";
-import Register from "./modules/Register";
-import TestOnLanding from "./modules/TestOnLanding";
+import Landing from "./modules/landing/Landing";
+import Test from "./modules/testView/Test";
+import Login from "./modules/auth/Login";
+import Register from "./modules/auth/Register";
+import TestOnLanding from "./modules/landing/TestOnLanding";
+import MagnifyBadge from "./modules/landing/MagnifyBadge";
+import SearchBar from "./modules/landing/SearchBar";
 
 const div = document.createElement("div");
 
@@ -15,32 +16,37 @@ it("renders without crashing", () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it(`Landing renders without crashing`, () => {
-  ReactDOM.render(<Landing />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
 it(`Login renders without crashing`, () => {
   ReactDOM.render(<Login />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-
-// it(`Navbar renders without crashing`, () => {
-//   ReactDOM.render(<Navbar />, <App />);
-//   ReactDOM.unmountComponentAtNode(<App />);
-// });
 
 it(`Register renders without crashing`, () => {
   ReactDOM.render(<Register />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it(`Test renders without crashing`, () => {
-  ReactDOM.render(<Test />, div);
+it(`Landing renders without crashing`, () => {
+  ReactDOM.render(<Landing />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it(`MagnifyBadge renders without crashing`, () => {
+  ReactDOM.render(<MagnifyBadge />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it(`SearchBar renders without crashing`, () => {
+  ReactDOM.render(<SearchBar />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it(`TestOnLanding renders without crashing`, () => {
   ReactDOM.render(<TestOnLanding />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it(`Test renders without crashing`, () => {
+  ReactDOM.render(<Test />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
